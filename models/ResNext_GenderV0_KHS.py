7e13910877f0a09f46585a542df7a0ea4c90c0eb
hs.kim <tiryul@gmail.com>
2022-10-25 14:58:00 +0000

    modify: model -> models / conflict cv2

--- models/ResNext_GenderV0_KHS.py
+++ models/ResNext_GenderV0_KHS.py
@@ -6,9 +6,9 @@ import torch.nn.functional as F
 import torchvision.models as models
 
 
-class ResNext_AgeV0_KHS(nn.Module):
+class ResNext_GenderV0_KHS(nn.Module):
     def __init__(self, number_of_classes: int):
-        super(ResNext_AgeV0_KHS, self).__init__()
+        super(ResNext_GenderV0_KHS, self).__init__()
         self.backborn = torch.hub.load(
             'pytorch/vision:v0.10.0', 'resnext50_32x4d', pretrained=True)
         # for p in self.backborn.parameters():
