9d02ff60b6016fac8160bdd464cdef84e512e6a7
22eming <gurrl96@gmail.com>
2022-10-31 05:32:19 +0000

    FIX: Label Fixed Version trainMask.py

--- train/trainMask.py
+++ train/trainMask.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 import torch
 import torch.nn as nn
 import torch.optim as optim
-# from torch.utils.tensorboard import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
@@ -22,11 +22,35 @@ from utils.CustomDataset import CustomDataset
 from models.ResNext_GenderV0_sawol import ResNext_GenderV0_SAWOL
 from utils.Utility import setSeedEverything, calcF1Score
 
+from sklearn.metrics import classification_report
+import torch.nn.functional as F
+from copy import deepcopy
+
+class FocalLoss(nn.Module):
+    def __init__(self, weight=None,
+                 gamma=2., reduction='mean'):
+        nn.Module.__init__(self)
+        self.weight = weight
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, input_tensor, target_tensor):
+        log_prob = F.log_softmax(input_tensor, dim=-1)
+        prob = torch.exp(log_prob)
+        return F.nll_loss(
+            ((1 - prob) ** self.gamma) * log_prob,
+            target_tensor,
+            weight=self.weight,
+            reduction=self.reduction
+        )
+
+def calMultiF1Score(true, pred) -> dict:
+    return classification_report(true, pred, zero_division=0, output_dict=True)
+
 def train(args):
 
     # setting
     setSeedEverything(args.seed)
-    
     # augmentation
     transform = A.Compose([
         A.RandomResizedCrop(
@@ -43,7 +67,7 @@ def train(args):
     df = pd.read_csv(args.train_csv)
     train_df, val_df, _, _ = train_test_split(df, df, test_size=0.2, random_state=args.seed)
     
-    train_dataset = CustomDataset(args.train_path, train_df.path.values, train_df.age_class.values, transforms=transform)
+    train_dataset = CustomDataset(args.train_path, train_df.path.values, train_df.mask_class.values, transforms=transform)
     train_loader = DataLoader(
         train_dataset, 
         batch_size=args.batch_size, 
@@ -52,7 +76,7 @@ def train(args):
         pin_memory=True,
         drop_last=True)
 
-    val_dataset = CustomDataset(args.train_path, val_df.path.values, val_df.age_class.values, transforms=transform)
+    val_dataset = CustomDataset(args.train_path, val_df.path.values, val_df.mask_class.values, transforms=transform)
     val_loader = DataLoader(
         val_dataset, 
         batch_size=args.batch_size, 
@@ -65,7 +89,7 @@ def train(args):
     model = ResNext_GenderV0_SAWOL().to(args.device)
 
     # loss & metric
-    criterion = nn.CrossEntropyLoss()
+    criterion = FocalLoss()
     optimizer = optim.Adam(
         model.parameters(),
         lr=args.lr,
@@ -74,13 +98,14 @@ def train(args):
     scheduler = None
 
     # logging
-    # logger = SummaryWriter(log_dir=args.log_dir)
+    logger = SummaryWriter(log_dir=args.log_dir)
     with open(os.path.join(args.save_dir, 'config.json'), 'w', encoding='utf-8') as f:
         json.dump(vars(args), f, ensure_ascii=False, indent=4)
 
     # train
     best_val_acc = 0
     best_val_loss = np.inf
+    best_f1_score = 0
     best_model = None
     for epoch in range(1, args.epochs+1):
         model.train()
@@ -104,8 +129,8 @@ def train(args):
             if (idx + 1) % args.log_interval == 0:
                 train_loss = loss_value / args.log_interval
                 train_acc = matches / args.batch_size / args.log_interval
-                # logger.add_scalar("Train/loss", train_loss, epoch * len(train_loader) + idx)
-                # logger.add_scalar("Train/accuracy", train_acc, epoch * len(train_loader) + idx)
+                logger.add_scalar("Train/loss", train_loss, epoch * len(train_loader) + idx)
+                logger.add_scalar("Train/accuracy", train_acc, epoch * len(train_loader) + idx)
 
                 loss_value = 0
                 matches = 0
@@ -116,6 +141,7 @@ def train(args):
             model.eval()
             val_loss_items = []
             val_acc_items = []
+            f1_score_items = []
             figure = None
             for val_data in val_loader:
                 inputs, labels = val_data['image'], val_data['label']
@@ -124,32 +150,32 @@ def train(args):
 
                 outs = model(inputs)
                 preds = torch.argmax(outs, dim=-1)
-                f1_score = calcF1Score(preds.detach().cpu(), labels.detach().cpu())
-
+                f1_score = calMultiF1Score(preds.detach().cpu(), labels.detach().cpu())
                 loss = criterion(outs, labels).item()
                 acc = (labels == preds).sum().item()
                 val_loss_items.append(loss)
                 val_acc_items.append(acc)
-
                 best_val_loss = min(best_val_loss, loss)
-                if acc > best_val_acc:
-                    best_val_acc = acc
-                    best_model = model
+                
+                f1_score_items.append(f1_score['weighted avg']['f1-score'])
+                
+            if sum(f1_score_items) / len(f1_score_items) > best_f1_score:
+                best_f1_score = sum(f1_score_items) / len(f1_score_items)
+                best_model = deepcopy(model)
             
             val_loss = np.sum(val_loss_items) / len(val_loader)
             val_acc = np.sum(val_acc_items) / len(val_dataset)
             best_val_loss = min(best_val_loss, val_loss)
 
-            # logger.add_scalar("Val/loss", val_loss, epoch)
-            # logger.add_scalar("Val/accuracy", val_acc, epoch)
+            logger.add_scalar("Val/loss", val_loss, epoch)
+            logger.add_scalar("Val/accuracy", val_acc, epoch)
 
-        print(f'EPOCH: {epoch}/{args.epochs} | train loss: {train_loss:.3f}, train acc: {train_acc:.3f} | val loss: {val_loss:.3f}, val acc: {val_acc:.3f} f1_score: {f1_score:.3f}')
-
+        print(f'EPOCH: {epoch}/{args.epochs} | train loss: {train_loss:.3f}, train acc: {train_acc:.3f} | val loss: {val_loss:.3f}, val acc: {val_acc:.3f}, f1 score: {best_f1_score}')
+    
     os.makedirs(f'{args.save_dir}', exist_ok=True)
     torch.save(best_model.state_dict(), f"{args.save_dir}/{args.epochs}_best_model.pth")
 
     print(f'best_val_acc: {best_val_acc} | best_val_loss: {best_val_loss:.3f}')
-
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -158,7 +184,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=999, help='random seed (default: 999)')
     parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train (default: 10)')
     parser.add_argument("--resize", type=list, default=[224, 224], help='resize size for image when training')
-    parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
+    parser.add_argument('--batch_size', type=int, default=128, help='input batch size for training (default: 64)')
     parser.add_argument('--valid_batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-3)')
     parser.add_argument('--val_ratio', type=float, default=0.2, help='ratio for validaton (default: 0.2)')
@@ -174,3 +200,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     train(args)
+    
