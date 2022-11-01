0df1c4bbbedd3f46a0f1fa5bde2dca3f387819f8
22eming <gurrl96@gmail.com>
2022-11-01 15:18:57 +0000

    ADD: add gender model

--- models/ResNext_GenderV0_sawol.py
+++ models/ResNext_GenderV0_sawol.py
@@ -5,9 +5,9 @@ import torch.nn.functional as F
 import torchvision.models as models
 
 
-class ResNext_GenderV0_SAWOL(nn.Module):
+class ResNext_GenderV0_SAWOL_mask(nn.Module):
     def __init__(self: int):
-        super(ResNext_GenderV0_SAWOL, self).__init__()
+        super(ResNext_GenderV0_SAWOL_mask, self).__init__()
         self.backborn = torch.hub.load(
             'pytorch/vision:v0.10.0', 'resnext50_32x4d', weights=models.ResNeXt50_32X4D_Weights.IMAGENET1K_V1)
         self.classifier = nn.Sequential(
@@ -34,6 +34,24 @@ class ResNext_GenderV0_SAWOL_age(nn.Module):
             nn.LeakyReLU(0.2),
             nn.Dropout(p=0.2),
             nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.backborn(x)
+        x = self.classifier(x)
+        return x
+
+class ResNext_GenderV0_SAWOL_gender(nn.Module):
+    def __init__(self: int):
+        super(ResNext_GenderV0_SAWOL_gender, self).__init__()
+        self.backborn = torch.hub.load(
+            'pytorch/vision:v0.10.0', 'resnext50_32x4d', weights=models.ResNeXt50_32X4D_Weights.IMAGENET1K_V1)
+        self.classifier = nn.Sequential(
+            nn.Linear(1000, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(p=0.2),
+            nn.Linear(512, 2),
         )
 
     def forward(self, x):
