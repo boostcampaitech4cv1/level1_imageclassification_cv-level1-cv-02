e43092498c4199e8d3c8a8c446f62c51ab9b6a9c
22eming <gurrl96@gmail.com>
2022-11-01 07:44:57 +0000

    ADD: add 5 age cut model

--- models/ResNext_GenderV0_sawol.py
+++ models/ResNext_GenderV0_sawol.py
@@ -22,3 +22,21 @@ class ResNext_GenderV0_SAWOL(nn.Module):
         x = self.backborn(x)
         x = self.classifier(x)
         return x
+    
+class ResNext_GenderV0_SAWOL_age(nn.Module):
+    def __init__(self: int):
+        super(ResNext_GenderV0_SAWOL_age, self).__init__()
+        self.backborn = torch.hub.load(
+            'pytorch/vision:v0.10.0', 'resnext50_32x4d', weights=models.ResNeXt50_32X4D_Weights.IMAGENET1K_V1)
+        self.classifier = nn.Sequential(
+            nn.Linear(1000, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(p=0.2),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.backborn(x)
+        x = self.classifier(x)
+        return x
