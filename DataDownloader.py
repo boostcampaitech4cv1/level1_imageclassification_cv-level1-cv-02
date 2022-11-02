43769ec48b4eb1bdd285e6606a98aa1b98b9bd68
HSKim <tiryul@gmail.com>
2022-11-02 05:11:24 +0000

    modify: datadownloader / fix error

--- DataDownloader.py
+++ DataDownloader.py
@@ -1,5 +1,7 @@
-!wget 'https://aistages-prod-server-public.s3.amazonaws.com/app/Competitions/000206/data/train.tar.gz'
-!wget 'https://aistages-prod-server-public.s3.amazonaws.com/app/Competitions/000206/data/code.tar.gz'
-!mkdir './data'
-!tar -zxvf './train.tar.gz' --directory='./data'
-!tar -zxvf './code.tar.gz' --directory='./data'
+import os
+
+os.system("wget https://aistages-prod-server-public.s3.amazonaws.com/app/Competitions/000206/data/train.tar.gz")
+os.system("wget https://aistages-prod-server-public.s3.amazonaws.com/app/Competitions/000206/data/code.tar.gz")
+os.system("mkdir ./data")
+os.system("tar -zxvf ./train.tar.gz --directory=./data")
+os.system("tar -zxvf ./code.tar.gz --directory='./data")
